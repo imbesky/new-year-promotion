e691a459c2fc5e75cf1093270e1de46bcc6c2742
besky <onlybesky@gmail.com>
2023-12-03 00:10:44 +0900

    feat: 방문 날짜, 방문 인원, 주문 저장 기능 구현

--- src/main/java/imbesky/promotion/repository/DataRepository.java
+++ src/main/java/imbesky/promotion/repository/DataRepository.java
@@ -1,19 +1,42 @@
 package imbesky.promotion.repository;
 
-import imbesky.promotion.domain.Visit;
+import imbesky.promotion.domain.Order;
+import imbesky.promotion.domain.VisitDate;
+import imbesky.promotion.domain.VisitNumber;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class DataRepository {
     // 데이터베이스 연동 없이 구현
-    private Visit visit;
+    private VisitDate visitDate;
+    private VisitNumber visitNumber;
+    private Order order;
 
-    public void save(final Visit visit) {
-        this.visit = visit;
+    public VisitDate saveVisitDate(final VisitDate visitDate) {
+        this.visitDate = visitDate;
+        return visitDate;
     }
 
-    public Visit inquireVisit() {
-        return visit;
+    public VisitNumber saveVisitNumber(final VisitNumber visitNumber) {
+        this.visitNumber = visitNumber;
+        return visitNumber;
+    }
+
+    public Order saveOrder(final Order order) {
+        this.order = order;
+        return order;
+    }
+
+    public VisitDate inquireVisitDate() {
+        return visitDate;
+    }
+
+    public VisitNumber inquireVisitNumber() {
+        return visitNumber;
+    }
+
+    public Order inquireOrder() {
+        return order;
     }
 
 }
---
+++ src/main/java/imbesky/promotion/service/InputService.java
@@ -0,0 +1,35 @@
+package imbesky.promotion.service;
+
+import imbesky.promotion.domain.Order;
+import imbesky.promotion.domain.VisitDate;
+import imbesky.promotion.domain.VisitNumber;
+import imbesky.promotion.domain.dto.InputDto;
+import imbesky.promotion.repository.DataRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InputService {
+    private final DataRepository repository;
+
+    public InputService(DataRepository repository) {
+        this.repository = repository;
+    }
+
+    public void save(final InputDto inputDto) {
+        final VisitDate visitDate = saveVisitDate(inputDto.date());
+        final VisitNumber visitNumber = saveVisitNumber(inputDto.number(), visitDate.isWeekday());
+        saveOrder(inputDto.order(), visitNumber.getNumber());
+    }
+
+    private VisitDate saveVisitDate(final String input) {
+        return repository.saveVisitDate(new VisitDate(input));
+    }
+
+    private VisitNumber saveVisitNumber(final int input, final boolean isWeekday) {
+        return repository.saveVisitNumber(new VisitNumber(input, isWeekday));
+    }
+
+    private void saveOrder(final String order, final int visitNumber) {
+        repository.saveOrder(new Order(order, visitNumber));
+    }
+}
