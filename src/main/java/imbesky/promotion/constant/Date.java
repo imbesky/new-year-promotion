3d051a52bb19e8b8325f049b15c570b8da044235
besky <onlybesky@gmail.com>
2023-12-03 20:46:48 +0900

    refactor: 주말에 해당하는 요일을 상수로 저장

--- src/main/java/imbesky/promotion/constant/Date.java
+++ src/main/java/imbesky/promotion/constant/Date.java
@@ -1,12 +1,17 @@
 package imbesky.promotion.constant;
 
+import static java.time.DayOfWeek.SATURDAY;
+import static java.time.DayOfWeek.SUNDAY;
+
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.List;
 
 public class Date {
     public final static int YEAR = 2024;
     private final static int JANUARY = 1;
     private final static int FEBRUARY = 2;
-    public final static int SPECIAL_EVENT_STANDARD = 5;
+    public final static List<DayOfWeek> WEEKEND = List.of(SATURDAY, SUNDAY);
     public final static LocalDate PROMOTION_START_DATE = LocalDate.of(YEAR, JANUARY, 1);
     public final static LocalDate PROMOTION_END_DATE = LocalDate.of(YEAR, FEBRUARY, 29);
     public final static LocalDate DDAY_EVENT_END_DATE = LocalDate.of(YEAR, FEBRUARY, 10);
