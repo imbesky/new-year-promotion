7cfee670973ad82ad983f21067b0c922b9a752da
besky <onlybesky@gmail.com>
2023-12-03 14:53:18 +0900

    feat: 주문 검증 기능 구현

--- src/main/java/imbesky/promotion/domain/Order.java
+++ src/main/java/imbesky/promotion/domain/Order.java
@@ -1,8 +1,12 @@
 package imbesky.promotion.domain;
 
+import static imbesky.promotion.constant.Format.BLANK;
 import static imbesky.promotion.constant.Format.COMMA;
 import static imbesky.promotion.constant.Format.DASH;
+import static imbesky.promotion.constant.Format.EMPTY;
 import static imbesky.promotion.constant.Number.FIRST_INDEX;
+import static imbesky.promotion.constant.Number.ORDER_MAX;
+import static imbesky.promotion.constant.Number.ORDER_MIN;
 import static imbesky.promotion.constant.Number.SECOND_INDEX;
 
 import imbesky.promotion.constant.Menu;
@@ -11,32 +15,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Order {
-    private Map<Menu, Integer> orders = new HashMap<>();
+    private final Map<Menu, Integer> orders = new HashMap<>();
+    private int totalOrderedNumber = 0;
 
-    private Order(final String input) {
-        String[] orders = input.split(COMMA);
+    public Order(final String input, final int visitNumber) {
+        String[] orders = input.replace(BLANK, EMPTY).split(COMMA);
         for (String order : orders) {
-            String[] detail = order.split(DASH);
-            validate(detail);
+            final String[] detail = order.split(DASH);
+            orderValidate(detail);
             save(detail);
         }
+        finalValidate(visitNumber);
     }
 
-    private void validate(final String[] detail) {
-        if (Menu.findByName(detail[FIRST_INDEX]) == null) {
-            throw new OrderException();
-        }
+    private void orderValidate(final String[] detail) {
+        final int orderNumber;
         try {
-            Integer.parseInt(detail[SECOND_INDEX]);
+            orderNumber = Integer.parseInt(detail[SECOND_INDEX]);
         } catch (NumberFormatException e) {
             throw new OrderException();
         }
-        // 0 이상임?
-        // 합계 인당 10 이상임?
+        final Menu menu = Menu.findByName(detail[FIRST_INDEX]);
+        if (menu == null
+                || orders.containsKey(menu)
+                || orderNumber < ORDER_MIN) {
+            throw new OrderException();
+        }
+    }
+
+    private void finalValidate(final int visitNumber) {
+        if (totalOrderedNumber / visitNumber < ORDER_MIN
+                || totalOrderedNumber / visitNumber < ORDER_MAX) {
+            throw new OrderException();
+        }
     }
 
     private void save(final String[] detail) {
-        orders.put(Menu.findByName(detail[FIRST_INDEX]), Integer.valueOf(detail[SECOND_INDEX]));
+        orders.put(Menu.findByName(detail[FIRST_INDEX]), Integer.parseInt(detail[SECOND_INDEX]));
+        totalOrderedNumber += Integer.parseInt(detail[SECOND_INDEX]);
     }
 
     public int orderedNumber(final Menu menu) {
